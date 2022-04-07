f1ae2d5b860ad7d8a14781d51e83983b3829673d
gushia1 <98963153+gushia1@users.noreply.github.com>
2022-04-07 17:35:57 +0800

    Update ks.js

--- ks.js
+++ ks.js
@@ -18,10 +18,10 @@ kuaishou.api_st=yyyyyyyyy;'
 重写：
 [task_local]
 #快手极速版
-38 9-19 * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js, tag=快手极速版, enabled=true
+38 9-19 * * * https://github.com/gushia1/jisu/blob/main/ks.js, tag=快手极速版, enabled=true
 [rewrite_local]
-appsupport/yoda/biz/info url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js
-ksapp/client/package/renew url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ksjsb.js
+appsupport/yoda/biz/info url script-request-header https://github.com/gushia1/jisu/blob/main/ks.js
+ksapp/client/package/renew url script-request-header https://github.com/gushia1/jisu/blob/main/ks.js
 [MITM]
 #IOS用第一个，安卓用第二个
 hostname = api.kuaisho*.com
